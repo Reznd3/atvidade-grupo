1e023a6648d745da0680a771f0da785550ab8926
Hugo Oliveira <146291968+githug-o@users.noreply.github.com>
2023-10-16 20:33:18 -0300

    Update editar.py

--- editar.py
+++ editar.py
@@ -4,4 +4,18 @@ def atualizar_linha(pessoas,email):
             pessoas[email]['Nome']=nome
             pessoas[email]['Telefone']=telefone
             pessoas[email]['Email']=email
-    
+            
+def criar_arquivo_csv():
+    with open('arquivo.csv', mode="w", newline="") as arquivo_csv:
+        writer=csv.writer(arquivo_csv)
+        writer.writerow(["Nome","Telefone","Email"])
+
+        for pessoa in pessoas:
+            writer.writerow([pessoa["Nome"], pessoa["Telefone"], pessoa["Email"]])
+
+def ler_dados_csv():
+    with open('arquivo.csv', mode="r", newline="") as arquivo_csv:
+        leitor_csv = csv.DictReader(arquivo_csv)
+        print(" Nome    |   Telefone    |   Email   ")
+        for linha in leitor_csv:
+            print(f"    {pessoa["Nome"]}    |   {pessoa["Telefone"]}    |   {pessoa["Email"]}    ")
