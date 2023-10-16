1f142dbc275a70b061eed5812cbbab6be4c1f683
vitotoso <silvacabralvitor1@gmail.com>
2023-10-16 20:29:29 -0300

    atualizar

---
+++ editar.py
@@ -0,0 +1,7 @@
+def atualizar_linha(pessoas,email):
+    for pessoa in pessoas:   
+        if email=='Email':
+            pessoas[email]['Nome']=nome
+            pessoas[email]['Telefone']=telefone
+            pessoas[email]['Email']=email
+    
